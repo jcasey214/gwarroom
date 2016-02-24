96dd2951fdc1390e5b7a34676f61374f46f0e3bb
Jonathan Casey <jon_casey@me.com>
2016-02-23 17:36:41 -0700

    add average response times

--- server/index.js
+++ server/index.js
@@ -9,39 +9,67 @@ var unirest = require('unirest');
 var server = http.Server(app);
 var io = require('socket.io')(server);
 
+var dbUrl = (process.env.MONGOLAB_URI || 'mongodb://localhost:27017/') + 'gwar';
+var promise = require('promise')
 
-app.get('/api/servers', function(request, response, next){
-  warroom(function(error, data){
-    response.json({servers: data.data})
+var database = {
+  connect: function(){
+    return new promise(function(resolve, reject){
+      mongo.MongoClient.connect('mongodb://localhost:27017/gwar', function(err, db){
+        if(err){
+          console.log(err);
+          reject(err);
+        }else{
+          resolve(db);
+        }
+      })
+    })
+  }
+}
+
+var result = {};
+function findAverage(arr){
+  arr.forEach(function(server, i){
+    if (result.hasOwnProperty(server.id)){
+      result[server.id].total += parseFloat(server.responseTime);
+      result[server.id].count += 1;
+    }else{
+      result[server.id] = {
+        total : parseFloat(server.responseTime),
+        count : 1,
+        avg : function(){
+          return this.total / this.count;
+        }
+      }
+    }
   })
-});
-
-
-app.get('api/servers/:id', function(request, response, next){
-
-});
-
-app.get('/test', function (request, response, next){
-  warroom(function(error, data){
-    response.send(data);
-  })
-})
+  return result;
+}
 
 io.on('connection', function (socket){
   warroom(function(error, data){
+    var average = findAverage(data.data);
+    database.connect().then(function(db){
+      db.collection('status').insert(data.data, function(error, response){
+      })
+      db.close();
+    })
+    for(var i = 0; i < data.data.length; i += 1){
+      for(key in average){
+        console.log(data.data[i]);
+        console.log(key);
+        console.log(average[key].avg());
+        if(parseInt(data.data[i].id) == parseInt(key)){
+          data.data[i].avg = average[key].avg();
+          console.log('here', data.data[i]);
+        }
+      }
+    }
     socket.emit('servers', data)
   })
 })
 
 app.use('/', express.static('public'));
-
-// app.get('/', function(req, res){
-//   res.send('hello world');
-//   console.log(__dirname);
-// })
-
-
-
 
 server.listen(port, function(){
   console.log('server is listening');
